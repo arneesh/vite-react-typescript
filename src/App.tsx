320146f4ddb77b73216019dbbe91d37275baca94
arneesh <arneeshaima@gmail.com>
2021-05-14 22:03:30 +0530

    testing husky

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Reactaaaas
+            Learn Reactaaaass
           </a>
           {' | '}
           <a
